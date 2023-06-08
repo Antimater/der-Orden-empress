c03a32bcdd49668912e16fb9246fe944e69276f7
Antimater <marceljung97@hotmail.de>
2023-06-08 15:40:54 +0200

    pathing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const path = require('path');
 const express = require('express');
 
 // Constants
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.sendFile('../index.html');
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 app.get("/add", (req, res) => {
