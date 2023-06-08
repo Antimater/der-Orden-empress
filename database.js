0c2e8789615dc9ca4a5e7df5f1feb7e8511ff2c2
Antimater <marceljung97@hotmail.de>
2023-06-08 15:01:11 +0200

    express html

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 var Parse = require('parse/node');
 
-Parse.initialize("APP_ID", "JS_KEY"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.initialize("qOYkd6t1zXosfO2E2AGyOUgQliQmsAcQlq69gzmK", "SEgeBkTEnaSVzq6hdDGsb5xW9fw9GgkzESjU7mJk"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 Parse.serverURL = 'https://parseapi.back4app.com/'
 
 async function addAmountToDatabase() {
