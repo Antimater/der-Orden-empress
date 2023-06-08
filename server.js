77e02a91a3c41e7afba536c253fb967d566beb06
Antimater <marceljung97@hotmail.de>
2023-06-08 15:33:49 +0200

    new

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.sendFile('./index.html');
+    res.sendFile('../index.html');
 });
 
 app.get("/add", (req, res) => {
