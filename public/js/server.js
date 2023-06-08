a6fcdd817c9744ea467bc8ed2c9cc8adf913565d
Antimater <marceljung97@hotmail.de>
2023-06-08 19:41:00 +0200

    es

--- public/js/server.js
+++ public/js/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '../index.html');
+    res.sendFile("/index.html");
 });
 
 app.get("/add", (req, res) => {
