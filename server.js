d196257a430671ddf4726fbeac75f18fa008cac6
Antimater <marceljung97@hotmail.de>
2023-06-08 15:43:02 +0200

    dir current

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get("/add", (req, res) => {
